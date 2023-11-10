2193a315714a5e54dfaf8078a2965708cd04943d
ksw13 <ksw48483@gmail.com>
2023-11-10 19:14:29 +0900

    test: 방문 날짜 범위 검증 테스트 작성

--- src/test/java/christmas/model/VisitDayTest.java
+++ src/test/java/christmas/model/VisitDayTest.java
@@ -7,12 +7,20 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class VisitDayTest {
-    
+
     @ParameterizedTest
     @ValueSource(strings = {"2a", "aaa", "3333a"})
     void 방문날짜가_숫자로만_이루어져_있는지_검증(String input) {
         assertThatThrownBy(() -> new VisitDay(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ExceptionMessage.NOT_NUMBER.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "32"})
+    void 방문날짜가_유효한_범위인지_검증(String input) {
+        assertThatThrownBy(() -> new VisitDay(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionMessage.NOT_IN_RANGE.getMessage());
     }
 }
