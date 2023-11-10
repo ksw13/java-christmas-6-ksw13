2e33887be8fe6c637f4e8e4ce1fb39e119b74157
ksw13 <ksw48483@gmail.com>
2023-11-10 21:31:29 +0900

    feat: 주문 형식 검증 기능 구현

--- src/main/java/christmas/model/Order.java
+++ src/main/java/christmas/model/Order.java
@@ -1,6 +1,21 @@
 package christmas.model;
 
+import christmas.util.ExceptionMessage;
+import christmas.util.MessageConst;
+import java.util.regex.Pattern;
+
 public class Order {
     public Order(String order) {
+        validate(order);
+    }
+
+    private void validate(String order) {
+        if (!validFormat(order)) {
+            throw new IllegalArgumentException(ExceptionMessage.NOT_VALID_FORMAT.getMessage());
+        }
+    }
+
+    private boolean validFormat(String order) {
+        return Pattern.matches(MessageConst.REGEXP_ORDER_FORMAT, order);
     }
 }
--- src/main/java/christmas/util/ExceptionMessage.java
+++ src/main/java/christmas/util/ExceptionMessage.java
@@ -2,7 +2,8 @@ package christmas.util;
 
 public enum ExceptionMessage {
     NOT_NUMBER("입력값은 숫자만 가능합니다."),
-    NOT_IN_RANGE("유효하지 않은 날짜입니다.");
+    NOT_IN_RANGE("유효하지 않은 날짜입니다."),
+    NOT_VALID_FORMAT("유효하지 않은 주문 형식입니다.");
 
     public static final String BASE_MESSAGE = "[ERROR] %s 다시 입력해 주세요.";
     private String message;
