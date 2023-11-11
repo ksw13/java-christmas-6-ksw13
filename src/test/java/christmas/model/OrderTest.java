7209aad9b52b4886ad82acb5887a4a596b28df22
ksw13 <ksw48483@gmail.com>
2023-11-11 12:40:27 +0900

    test: 주문 수량 검증 테스트 작성

--- src/test/java/christmas/model/OrderTest.java
+++ src/test/java/christmas/model/OrderTest.java
@@ -8,12 +8,29 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class OrderTest {
     @ParameterizedTest
-    @ValueSource(strings = {"시저샐러드-1, 티본스테이크-1,", "시저샐러드-1-", "시저샐러드-1, 티본스테이크-110", "시저샐러드-1, 티본스테이크", "123",
+    @ValueSource(strings = {"시저샐러드-1,티본스테이크-1,", "시저샐러드-1-", "시저샐러드-1, 티본스테이크", "123",
             "시저샐러드-1개"})
     void 주문이_올바른_형식인지_검증(String input) {
         assertThatThrownBy(() -> new Order(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ExceptionMessage.NOT_VALID_FORMAT.getMessage());
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"티본스테이크-0,바비큐립-1,초코케이크-2,제로콜라-1", "시저샐러드-0"})
+    void 메뉴의_개수가_양의정수가_맞는지_검증(String input) {
+        assertThatThrownBy(() -> new Order(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionMessage.NOT_VALID_QUANTITY.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"시저샐러드-11,티본스테이크-10"})
+    void 메뉴의_총개수가_20이_넘는지_검증(String input) {
+        assertThatThrownBy(() -> new Order(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionMessage.NOT_VALID_TOTAL_QUANTITY.getMessage());
+    }
+
 
 }
